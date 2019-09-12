684be59cb2948ce2c941565bf69bdc168561c285
liam yafuso <liamyafuso@thefractalway.com>
2019-09-12 16:32:23 -0700

    add airline query

--- lib/flightstats/airline.rb
+++ lib/flightstats/airline.rb
@@ -6,5 +6,17 @@ module FlightStats
                   :name,
                   :phone_number,
                   :active
+
+    @@base_path = "/flex/airlines/rest/v1/json"
+
+    class << self
+      def actives(options = {})
+        from_response API.get("#{base_path}/active", {}, options), 'airlines'
+      end
+
+      def base_path
+        @@base_path
+      end
+    end
   end
 end
