79347f9f0768a66d61bd3e9b5cbf2387d30bbc07
dvajevec <dvajevec@theprivatesuite.com>
2020-03-20 11:13:06 -0700

    update operator props

--- lib/flightstats/operator.rb
+++ lib/flightstats/operator.rb
@@ -1,5 +1,5 @@
 module FlightStats
   class Operator < Resource
-    attr_accessor :carrier_fs_code
+    attr_accessor :carrier_fs_code, :flight_number, :service_type, :service_classes, :traffic_restrictions
   end
 end
